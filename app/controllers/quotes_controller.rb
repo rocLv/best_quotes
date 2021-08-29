bc02ba59e65e2295d6c3abf2400459fcf56056f0
rocLv <wangqsh999@gmail.com>
2021-08-29 08:58:25 +0800

    add controller and autoload

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,6 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing eigher good or bad " +
+      "but thinking make it so."
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,8 @@
+require "rulers"
+
+$LOAD_PATH << "/Users/i/Code/iClean/P7RubyTraining/best_quotes/app/controllers"
+
+module BestQuotes
+  class Application < Rulers::Application
+  end
+end
